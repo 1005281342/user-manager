98cb78f5a86bb181e8ea20d00ba0ade78d124b1f
1005281342 <1005281342@qq.com>
2023-04-29 01:20:14 +0800

    feat: 优化初始化脚本

--- replit-init.sh
+++ replit-init.sh
@@ -1,6 +1,12 @@
-export BINDIR=.
-export BINARY=bud-run
-curl -sf https://raw.githubusercontent.com/livebud/bud/main/install.sh | sh
-chmod +x ./bud-run
+if test -f "bud-run"; then
+  echo "File bud-run exists."
+else
+  echo "File bud-run does not exist."
+  rm -rf bud
+  export BINDIR=.
+  curl -sf https://raw.githubusercontent.com/livebud/bud/main/install.sh | sh
+  chmod +x ./bud-run
+fi
+
 npm install
 go mod tidy
